239665dbe3565b9f9ae3ace4386e8bb1352ac1a0
Tyrone <Tyrone@192.168.0.102>
2016-11-04 12:48:36 -0600

    Added WebServer class

---
+++ src/cpsc441_assignment2/WebServer.java
@@ -0,0 +1,12 @@
+package cpsc441_assignment2;
+
+public class WebServer extends Thread {
+	private int _Port;
+	public WebServer(int port){
+		_Port = port;
+	}
+	
+	public void shutdown(){
+		
+	}
+}
